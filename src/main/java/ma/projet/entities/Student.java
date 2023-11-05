91bb58030c2d73972b77d5f194a96d57153a09c8
Leeuw <mohamedessabir20@gmail.com>
2023-11-05 17:19:54 +0100

    First commit

---
+++ src/main/java/ma/projet/entities/Student.java
@@ -0,0 +1,54 @@
+package ma.projet.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Student extends User {
+	
+	private String name;
+	private String phone;
+	private String email;
+
+	@ManyToOne
+	private Filiere filiere;
+
+	public Filiere getFiliere() {
+		return filiere;
+	}
+
+	public void setFiliere(Filiere filiere) {
+		this.filiere = filiere;
+	}
+
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
